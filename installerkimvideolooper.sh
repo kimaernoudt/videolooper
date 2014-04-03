a34088d6e24ce340a9b4156c34c60a4ec0def76a
kimaernoudt <kimaernoudt@users.noreply.github.com>
2014-04-04 00:13:05 +0200

    Update installerkimvideolooper.sh

--- installerkimvideolooper.sh
+++ installerkimvideolooper.sh
@@ -9,6 +9,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # copy the needed files
+echo "copying the files"
 cp -a startvideos.sh /home/pi/
 cp -a startfullscreen.sh /home/pi/
 cp -a videoloop /etc/init.d/
@@ -16,13 +17,19 @@ cp -a 10-usbdriveadded.rules /etc/udev/rules.d/
 cp -a 10-usbdriveremoved.rules /etc/udev/rules.d/
 cp -a startstartloop.sh /home/pi/
 cp -a stoploop.sh /home/pi
+echo "copy succes"
 
+echo "editing fstab to automount usbdrive on /mnt/media/usbdrive"
 # edit fstab
 echo '/mnt/media/usbdrive/ vfat rw,defaults 0 0' >> /etc/fstab
+echo "editing fstab succes"
 
 #update rc.d with the new daemon
+echo "updating rc.d"
 update-rc.d videoloop defaults
+echo "updating rc.d succes"
 
+echo "this raspberry is now an automatic videolooper, it plays from the usbstick, default audio is trough hdmi"
 
 
 
